d4262051dfbe7007ed7128e9d878895512507894
santisebas97 <nunezsantiago1e@gmail.com>
2022-01-10 17:20:39 -0500

    Initial commit

---
+++ app/src/main/java/fisei/santiago_nunez_2doparcial_prueba01/MainActivity.java
@@ -0,0 +1,14 @@
+package fisei.santiago_nunez_2doparcial_prueba01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
